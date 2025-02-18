1cc351269b0e6efb9778ef0457f2c3aee80a777a
MeanPaper <ldm1833991255@gmail.com>
2025-02-18 13:14:51 -0600

    avatars

--- src/AvatarLinks.js
+++ src/AvatarLinks.js
@@ -1,16 +1,16 @@
 const AvatarLinks = {
     "Dongming Liu":{
-        github: 'https://meanpaper.github.io/assets/img/dongming-480.webp',
+        github: 'https://github.com/MeanPaper/MeanPaper.github.io/blob/master/assets/img/dongming.png',
         // github:'https://www.github.com/MeanPaper.png',
     },
     "Elijah Ye":{
         github: `https://www.github.com/Elijah-Ye.png`,
     },
     "Owen Xu":{
-        linkedin: `https://media.licdn.com/dms/image/v2/D5635AQGIpIHXWsFBNA/profile-framedphoto-shrink_400_400/profile-framedphoto-shrink_400_400/0/1707106987201?e=1727935200&v=beta&t=pKQeZwuHGdvKvLWagub1qMVdG6FCAI4J7XWbP9pm6Tc`,
+        linkedin: ``,
     },
     "Xinyu Wu":{
-        linkedin: `https://media.licdn.com/dms/image/v2/D5603AQEp0YaOWxwx_A/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1706397699517?e=1732752000&v=beta&t=Cn22FQtD3pN-1oeaKY20VM-VbRLybffmP-8N6iegukY`,
+        linkedin: ``,
     },
 };
 
